e8a468f77faec78901c5ceea0aeff01972a3a244
Silver Valdvee <silver_valdvee@hotmail.com>
2021-12-02 16:59:28 +0200

    Support variable bit amount

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-function hex2bin(hex) {
-    return (parseInt(hex, 16).toString(2)).padStart(32, '0');
+function hex2bin(hex, pad) {
+    return (parseInt(hex, 16).toString(2)).padStart(pad, '0');
 }
-function bin2hex(bin) {
-    return (parseInt(bin, 2).toString(16)).padStart(8, '0');
+function bin2hex(bin, pad) {
+    return (parseInt(bin, 2).toString(16)).padStart(pad, '0');
 }
 window.addEventListener('load', function () {
+    let bitCount = document.querySelector("#bitCount");
+    let bitsCount = bitCount.value;
+    let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    bitCount.addEventListener('keypress', function (e) {
+        if (!numbers.includes(e.key)) {
+            console.log("does not contain")
+            e.preventDefault();
+            return;
+        }
+        setTimeout(() => {
+            bitsCount = bitCount.value
+            bitCount.Value = bitsCount
+            refresh();
+        }, 30);
+    });
     let bitsContainer = document.querySelector("#bits-container");
     let numberingContainer = document.querySelector("#numberingContainer");
     let rbitsContainer = document.querySelector("#rbits-container");
@@ -21,7 +36,7 @@ window.addEventListener('load', function () {
 
     btnOutputBinaryLiteral.addEventListener("click", function () {
         let gatheredValue = "0b";
-        for (let i = 0; i < 32; i++) {
+        for (let i = 0; i < bitsCount; i++) {
             let ithElement = document.querySelector("#i" + i);
             gatheredValue += ithElement.value;
         }
@@ -31,34 +46,34 @@ window.addEventListener('load', function () {
 
     btnOutputHexLiteral.addEventListener("click", function () {
         let gatheredValue = "";
-        for (let i = 0; i < 32; i++) {
+        for (let i = 0; i < bitsCount; i++) {
             let ithElement = document.querySelector("#i" + i);
             gatheredValue += ithElement.value;
         }
-        outputHere.value = "0x" + bin2hex(gatheredValue);
+        outputHere.value = "0x" + bin2hex(gatheredValue, Math.ceil(bitsCount / 8));
         outputLabel.textContent = "This is your hex literal output";
     });
     function syncFromNormal() {
-        for (let i = 0; i < 32; i++) {
+        for (let i = 0; i < bitsCount; i++) {
             let ithElement = document.querySelector("#i" + i);
             let rithElement = document.querySelector("#ri" + i);
             rithElement.value = ithElement.value;
         }
     }
     function syncFromReverse() {
-        for (let i = 0; i < 32; i++) {
+        for (let i = 0; i < bitsCount; i++) {
             let ithElement = document.querySelector("#i" + i);
             let rithElement = document.querySelector("#ri" + i);
             ithElement.value = rithElement.value;
         }
     }
     btnConvertToBinary.addEventListener('click', function () {
-        let convertedString = hex2bin(hexHere.value)
-        for (let i = 0; i < 32; i++) {
+        let convertedString = hex2bin(hexHere.value, bitsCount)
+        for (let i = 0; i < bitsCount; i++) {
             let ithElement = document.querySelector("#i" + i);
             ithElement.value = convertedString[i];
         }
-        for (let i = 31; i >= 0; i--) {
+        for (let i = bitsCount - 1; i >= 0; i--) {
             let ithElement = document.querySelector("#ri" + i);
             ithElement.value = convertedString[i];
         }
@@ -66,58 +81,70 @@ window.addEventListener('load', function () {
 
     btnPushBinary.addEventListener('click', function () {
         let convertedString = binHere.value.replace("0b", "");
-        for (let i = 0; i < 32; i++) {
+        for (let i = 0; i < bitsCount; i++) {
             let ithElement = document.querySelector("#i" + i);
             ithElement.value = convertedString[i];
         }
-        for (let i = 31; i >= 0; i--) {
+        for (let i = bitsCount - 1; i >= 0; i--) {
             let ithElement = document.querySelector("#ri" + i);
             ithElement.value = convertedString[i];
         }
     });
-    for (let i = 0; i < 32; i++) {
-        console.log(i);
-        let inputElement = document.createElement("input");
-        inputElement.type = "text";
-        inputElement.id = "i" + i
-        inputElement.className = "small-input";
-        inputElement.setAttribute("maxlength", 1);
-        bitsContainer.appendChild(inputElement);
-        inputElement.addEventListener('keypress', function (e) {
-            if (e.key !== "1" && e.key !== "0") {
-                e.preventDefault();
-            }
-            setTimeout(() => {
-                syncFromNormal();
-            }, 10);
-        });
-        let numberingElement = document.createElement("input");
-        numberingElement.className = "small-input";
-        numberingElement.setAttribute("disabled", true)
-        numberingElement.value = 31 - i
-        numberingContainer.appendChild(numberingElement);
+    function removeAllChildNodes(parent) {
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
     }
-    for (let i = 31; i >= 0; i--) {
-        console.log(i);
-        let inputElement = document.createElement("input");
-        inputElement.type = "text";
-        inputElement.id = "ri" + i
-        inputElement.className = "small-input";
-        inputElement.setAttribute("maxlength", 1);
-        rbitsContainer.appendChild(inputElement);
-        inputElement.addEventListener('keypress', function (e) {
-            if (e.key !== "1" && e.key !== "0") {
-                e.preventDefault();
+    function refresh() {
+        removeAllChildNodes(bitsContainer)
+        removeAllChildNodes(numberingContainer)
+        removeAllChildNodes(rbitsContainer)
+        removeAllChildNodes(rnumberingContainer);
+        setTimeout(() => {
+            for (let i = 0; i < bitsCount; i++) {
+                let inputElement = document.createElement("input");
+                inputElement.type = "text";
+                inputElement.id = "i" + i
+                inputElement.className = "small-input";
+                inputElement.setAttribute("maxlength", 1);
+                bitsContainer.appendChild(inputElement);
+                inputElement.addEventListener('keypress', function (e) {
+                    if (e.key !== "1" && e.key !== "0") {
+                        e.preventDefault();
+                    }
+                    setTimeout(() => {
+                        syncFromNormal();
+                    }, 10);
+                });
+                let numberingElement = document.createElement("input");
+                numberingElement.className = "small-input";
+                numberingElement.setAttribute("disabled", true)
+                numberingElement.value = bitsCount - 1 - i
+                numberingContainer.appendChild(numberingElement);
             }
-            setTimeout(() => {
-                syncFromReverse();
-            }, 10);
+            for (let i = bitsCount - 1; i >= 0; i--) {
+                let inputElement = document.createElement("input");
+                inputElement.type = "text";
+                inputElement.id = "ri" + i
+                inputElement.className = "small-input";
+                inputElement.setAttribute("maxlength", 1);
+                rbitsContainer.appendChild(inputElement);
+                inputElement.addEventListener('keypress', function (e) {
+                    if (e.key !== "1" && e.key !== "0") {
+                        e.preventDefault();
+                    }
+                    setTimeout(() => {
+                        syncFromReverse();
+                    }, 10);
+                });
+                let numberingElement = document.createElement("input");
+                numberingElement.className = "small-input";
+                numberingElement.setAttribute("disabled", true)
+                numberingElement.value = bitsCount - 1 - i
+                rnumberingContainer.appendChild(numberingElement);
+            }
+        }, 10);
 
-        });
-        let numberingElement = document.createElement("input");
-        numberingElement.className = "small-input";
-        numberingElement.setAttribute("disabled", true)
-        numberingElement.value = 31 - i
-        rnumberingContainer.appendChild(numberingElement);
     }
+    refresh();
 });
